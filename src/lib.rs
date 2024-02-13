5ba88d08fb3c4b361be7f357993fe9d4c1776623
Dominik Böhler <dominik.boehler@gmx.net>
2024-02-13 21:26:42 +0100

    first

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,7 @@
 /// Determine whether a sentence is a pangram.
 pub fn is_pangram(sentence: &str) -> bool {
-    unimplemented!("Is {sentence} a pangram?");
+    let hashed = HashSet::from(sentence);
+    let alphabet = HashSet::from((b'a'..=b'z').collect()).unwrap();
+    hashed == alphabet
+
 }
