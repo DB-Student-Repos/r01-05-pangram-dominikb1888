75f2e8ec65781a0e7c02ca49067845646b3830e2
Dominik Böhler <dominik.boehler@gmx.net>
2024-02-13 21:57:51 +0000

    abc

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 use std::collections::HashSet;
 /// Determine whether a sentence is a pangram.
 pub fn is_pangram(sentence: &str) -> bool {
-    let hashed = HashSet::from(sentence.into_iter());
+    let hashed = HashSet::from(sentence.to_vec());
     let alphabet = HashSet::from((b'a'..=b'z').collect().unwrap());
     hashed == alphabet
 
